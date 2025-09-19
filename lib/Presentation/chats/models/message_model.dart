10f036c7e8a185561bc1a619d4fac47af6d0dbf8
raniamohamed <ranyamohamed777999@gmail.com>
2025-09-19 04:21:59 +0300

    chat_feature

---
+++ lib/Presentation/chats/models/message_model.dart
@@ -0,0 +1,23 @@
+class Message {
+  final String text;
+  final bool isMe;
+  final String time;
+
+  Message({
+    required this.text,
+    required this.isMe,
+    required this.time,
+  });
+}
+
+List<Message> demoMessages(String chatName) => [
+      Message(text: "Hi, $chatName 🙏", isMe: true, time: "09:10"),
+      Message(text: "Good morning, how can I help you?", isMe: false, time: "09:40"),
+      Message(text: "Good morning doctor, I have a headache and body aches.", isMe: true, time: "09:40"),
+      Message(text: "Fine, how long has the complaint been?", isMe: false, time: "09:40"),
+      Message(text: "It’s been about the last 3 days.", isMe: true, time: "09:40"),
+      Message(
+          text: "Fine, I’ll do a check. Does the patient have a history of certain diseases?",
+          isMe: false,
+          time: "09:40"),
+    ];
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import emoji_picker_flutter
+import file_picker
+import file_selector_macos
 import path_provider_foundation
+import record_macos
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
